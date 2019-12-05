4c40c0940602728c41d4038faf00f4df6cc75adf
Zannatul ferdous Prome <47224433+PromeZannat@users.noreply.github.com>
2019-12-05 22:23:18 +0600

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,184 +1,49 @@
 package com.example.androidmusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.speech.RecognitionListener;
-import android.speech.RecognizerIntent;
-import android.speech.SpeechRecognizer;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.Locale;
+import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity
-{
-    private RelativeLayout parentRelativeLayout;
-    private SpeechRecognizer speechRecognizer;
-    private Intent speechRecognizerIntent;
-    private String keeper = "";
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-    private ImageView pausePlayBtn, nextBtn, previousBtn;   //**Added play pause button
-    private TextView songNameTxt;
-
-    private ImageView imageView;
-    private RelativeLayout lowerRelativeLayout;
-    private Button voiceEnabledBtn;
-    private String mode = "ON";
+    private EditText signInEmailEditText, signInPasswordEditText;
+    private TextView signUpTextView;
+    private Button signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        signInEmailEditText = findViewById(R.id.signInEmailEditTextId);
+        signInPasswordEditText= findViewById(R.id.signInPasswordEditTextId);
+        signInButton = findViewById(R.id.signInButtonId);
+        signUpTextView = findViewById(R.id.signUpTextViewId);
 
-        checkVoiceCommandPermission();
-
-
-        pausePlayBtn = findViewById(R.id.play_pause_btn);
-        nextBtn = findViewById(R.id.next_btn);
-        previousBtn = findViewById(R.id.previous_btn);
-        imageView = findViewById(R.id.logo);
-
-        lowerRelativeLayout = findViewById(R.id.lower);
-        voiceEnabledBtn = findViewById(R.id.voice_enabled_btn);
-        songNameTxt = findViewById(R.id.songName);
-
-        parentRelativeLayout = findViewById(R.id.parentRelativeLayout);
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(MainActivity.this);
-        speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-
-
-
-        speechRecognizer.setRecognitionListener(new RecognitionListener() {
-            @Override
-            public void onReadyForSpeech(Bundle params) {
-
-            }
-
-            @Override
-            public void onBeginningOfSpeech() {
-
-            }
-
-            @Override
-            public void onRmsChanged(float rmsdB) {
-
-            }
-
-            @Override
-            public void onBufferReceived(byte[] buffer) {
-
-            }
-
-            @Override
-            public void onEndOfSpeech() {
-
-            }
-
-            @Override
-            public void onError(int error) {
-
-            }
-
-            @Override
-            public void onResults(Bundle results)
-            {
-                ArrayList<String> matchesFound = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-                if (matchesFound != null)
-                {
-                    keeper = matchesFound.get(0);
-                    Toast.makeText(MainActivity.this, "Result" + keeper, Toast.LENGTH_LONG).show();
-                }
-
-            }
-
-            @Override
-            public void onPartialResults(Bundle partialResults) {
-
-            }
-
-            @Override
-            public void onEvent(int eventType, Bundle params) {
-
-            }
-        });
-
-
-       parentRelativeLayout.setOnTouchListener(new View.OnTouchListener() {
-           @Override
-           public boolean onTouch(View view, MotionEvent event)
-           {
-               switch (event.getAction())
-               {
-                   case MotionEvent.ACTION_DOWN:
-                       speechRecognizer.startListening(speechRecognizerIntent);
-                       keeper = "";
-                       break;
-                   case MotionEvent.ACTION_UP:
-                       speechRecognizer.stopListening();
-                       break;
-               }
-
-               return false;
-
-           }
-       });
-
-
-
-       voiceEnabledBtn.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v)
-           {
-                if (mode.equals("ON"))
-                {
-                    mode = "OFF";
-                    voiceEnabledBtn.setText("Voice Enable Mode - OFF");
-                    lowerRelativeLayout.setVisibility(View.VISIBLE);
-                }
-                else
-                {
-                    mode = "ON";
-                    voiceEnabledBtn.setText("Voice Enable Mode - ON");
-                    lowerRelativeLayout.setVisibility(View.GONE);
-                }
-
-           }
-       });
-
+        signUpTextView.setOnClickListener(this);
+        signInButton.setOnClickListener(this);
 
     }
 
-
-
-
-    private void checkVoiceCommandPermission()
-    {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-        {
-            if (!(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED))
-            {
-                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" +getPackageName()));
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.signInButtonId:
+                break;
+            case R.id.signUpTextViewId:
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
                 startActivity(intent);
-                finish();
-            }
+                break;
         }
+
     }
 }
