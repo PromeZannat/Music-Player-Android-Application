8b0ded154e6700f4bd63075c7bef5a70947b0200
Zannatul ferdous Prome <47224433+PromeZannat@users.noreply.github.com>
2019-12-05 22:37:26 +0600

    Add files via upload

--- SignUpActivity.java
+++ SignUpActivity.java
@@ -2,13 +2,48 @@ package com.example.androidmusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class SignUpActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private EditText signUpEmailEditText, signUpPasswordEditText;
+    private TextView signInTextView;
+    private Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        signUpEmailEditText = findViewById(R.id.signUpEmailEditTextId);
+        signUpPasswordEditText= findViewById(R.id.signUpPasswordEditTextId);
+        signUpButton = findViewById(R.id.signUpButtonId);
+        signInTextView = findViewById(R.id.signInTextViewId);
+
+        signInTextView.setOnClickListener(this);
+        signUpButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.signUpButtonId:
+                break;
+            case R.id.signInTextViewId:
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                break;
+        }
+
     }
 }
