b55e7a2865ab4d495115eaa0a0ac61b250142508
Zannatul ferdous Prome <47224433+PromeZannat@users.noreply.github.com>
2019-12-06 18:35:09 +0600

    Update SignUpActivity.java

--- SignUpActivity.java
+++ SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidmusicplayer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
@@ -8,7 +9,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.w3c.dom.Text;
 
@@ -18,6 +26,8 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     private EditText signUpEmailEditText, signUpPasswordEditText;
     private TextView signInTextView;
     private Button signUpButton;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +35,13 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_sign_up);
         this.setTitle("Sign Up Activity");
 
+        mAuth = FirebaseAuth.getInstance();
+
         signUpEmailEditText = findViewById(R.id.signUpEmailEditTextId);
         signUpPasswordEditText= findViewById(R.id.signUpPasswordEditTextId);
         signUpButton = findViewById(R.id.signUpButtonId);
         signInTextView = findViewById(R.id.signInTextViewId);
+        progressBar = findViewById(R.id.progressBarId);
 
         signInTextView.setOnClickListener(this);
         signUpButton.setOnClickListener(this);
@@ -52,7 +65,44 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     private void userRegister() {
         String email = signUpEmailEditText.getText().toString().trim();
         String password =  signUpPasswordEditText.getText().toString().trim();
+        if(email.isEmpty())
+        {
+            signUpEmailEditText.setError("Enter an email address");
+            signUpEmailEditText.requestFocus();
+            return;
+        }
 
+        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            signUpEmailEditText.setError("Enter a valid email address");
+            signUpEmailEditText.requestFocus();
+            return;
+        }
 
+        //checking the validity of the password
+        if(password.isEmpty())
+        {
+            signUpPasswordEditText.setError("Enter a password");
+            signUpPasswordEditText.requestFocus();
+            return;
+        }
+        if(password.length()<6)
+        {
+            signUpPasswordEditText.setError("Minimum length of a password should be 6");
+            signUpPasswordEditText.requestFocus();
+            return;
+        }
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Registration Successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Registration not Successful", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
