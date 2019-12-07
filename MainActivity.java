86e44b2266e61f7f86a4befb0454bbc48f9ca401
Zannatul ferdous Prome <47224433+PromeZannat@users.noreply.github.com>
2019-12-07 23:56:05 +0600

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidmusicplayer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
@@ -8,7 +9,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 import org.w3c.dom.Text;
 
@@ -18,6 +27,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText signInEmailEditText, signInPasswordEditText;
     private TextView signUpTextView;
     private Button signInButton;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +36,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         this.setTitle("Sign In Activity");
 
+        mAuth = FirebaseAuth.getInstance();
+
         signInEmailEditText = findViewById(R.id.signInEmailEditTextId);
         signInPasswordEditText= findViewById(R.id.signInPasswordEditTextId);
         signInButton = findViewById(R.id.signInButtonId);
         signUpTextView = findViewById(R.id.signUpTextViewId);
+        progressBar = findViewById(R.id.progressBarId);
 
         signUpTextView.setOnClickListener(this);
         signInButton.setOnClickListener(this);
@@ -39,12 +53,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.signInButtonId:
+                userLogin();
                 break;
             case R.id.signUpTextViewId:
                 Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
                 startActivity(intent);
                 break;
         }
+
+    }
+
+    private void userLogin() {
+        String email = signInEmailEditText.getText().toString().trim();
+        String password =  signInPasswordEditText.getText().toString().trim();
+        if(email.isEmpty())
+        {
+            signInEmailEditText.setError("Enter an email address");
+            signInEmailEditText.requestFocus();
+            return;
+        }
+
+        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            signInEmailEditText.setError("Enter a valid email address");
+            signInEmailEditText.requestFocus();
+            return;
+        }
+
+        //checking the validity of the password
+        if(password.isEmpty())
+        {
+            signInPasswordEditText.setError("Enter a password");
+            signInPasswordEditText.requestFocus();
+            return;
+        }
+        if(password.length()<6)
+        {
+            signInPasswordEditText.setError("Minimum length of a password should be 6");
+            signInPasswordEditText.requestFocus();
+            return;
+        }
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                
+            }
+        });
 
     }
 }
