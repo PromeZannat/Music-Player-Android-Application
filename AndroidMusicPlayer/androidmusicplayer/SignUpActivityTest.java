90df81a81d35cf8d015c12461df28e37536c267f
Zannatul ferdous Prome <47224433+PromeZannat@users.noreply.github.com>
2019-12-17 23:46:43 +0600

    Update SignUpActivityTest.java

--- AndroidMusicPlayer/androidmusicplayer/SignUpActivityTest.java
+++ AndroidMusicPlayer/androidmusicplayer/SignUpActivityTest.java
@@ -2,16 +2,31 @@ package com.example.androidmusicplayer;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SignUpActivityTest {
 
+    @Rule
+    public ActivityTestRule<SignUpActivity> mActivityTestRule = new ActivityTestRule<SignUpActivity>(SignUpActivity.class);
+
+    private SignUpActivity mActivity = null;
+    
     @Before
     public void setUp() throws Exception {
+        mActivity = mActivityTestRule.getActivity;
+    }
+
+    @Test
+    public void testLaunch()
+    {
+
     }
 
     @After
     public void tearDown() throws Exception {
+        mActivity = null;
     }
 }
